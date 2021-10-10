725b2b6a84126046fb9c7f1c52abea6fcd9e9b9a
mcastaneda82 <mcastaneda82@toromail.csudh.edu>
2021-10-10 13:11:04 -0700

    Added timestamp to logs.

--- src/utils/devLog.js
+++ src/utils/devLog.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 
 const devLog = (string) => {
-  if (process.env.NODE_ENV = 'development') console.log('📋 DEV LOG: ' + string)
+  if (process.env.NODE_ENV = 'development') {
+    let currentTime = Date.now()
+    currentTime -= 60000 * 60 * 7
+    const formattedTime = new Date(currentTime).toLocaleString('en-US', { timezone: 'UTC' })
+    console.log(`📋 ${formattedTime} DEV LOG: ${string}`)
+  }
 }
 
 module.exports = devLog
